3e058f93cf1fb07292b783b3877dc8902b99e617
trontram <txtron2012@gmail.com>
2022-09-11 11:54:19 +0700

    update deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :rbenv_custom_path, "/home/deploy/.rbenv"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/deploy/apps/learn_ci_cd"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
